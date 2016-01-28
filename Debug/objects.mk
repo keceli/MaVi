4287336c50e4a4da465884f6c91e442dacb6aae3
Murat Keceli <keceli@gmail.com>
2016-01-28 13:36:34 -0600

    Added makefiles generated by Eclipse

---
+++ Debug/objects.mk
@@ -0,0 +1,11 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+#LIBS :=  -Wl,--start-group $(MKLROOT)/lib/libmkl_intel_ilp64.a $(MKLROOT)/lib/libmkl_intel_thread.a $(MKLROOT)/lib/libmkl_core.a -Wl,--end-group -openmp -lpthread 
+#LIBS := -lmkl_rt -openmp -lpthread 
+LIBS := -L/Volumes/s/keceli/lib/petsc/dev_debug/lib -llapack -lblas 
+#LIBS := -lacml
+
---
+++ Debug/src/subdir.mk
@@ -0,0 +1,44 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+F90_SRCS += \
+../src/1global.f90 \
+../src/1tools.f90 \
+../src/3qff.f90 \
+../src/3states.f90 \
+../src/4formPES.f90 \
+../src/4integral.f90 \
+../src/5modal.f90 \
+../src/6oldvscf.f90 \
+../src/6vscf.f90 \
+../src/6xvscf.f90 \
+../src/7vci.f90 \
+../src/7vpt.f90 \
+../src/main.f90 
+
+OBJS += \
+./src/1global.o \
+./src/1tools.o \
+./src/3qff.o \
+./src/3states.o \
+./src/4formPES.o \
+./src/4integral.o \
+./src/5modal.o \
+./src/6oldvscf.o \
+./src/6vscf.o \
+./src/6xvscf.o \
+./src/7vci.o \
+./src/7vpt.o \
+./src/main.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.f90
+	@echo 'Building file: $<'
+	gfortran -c -std=legacy -Wall -Wextra -Wimplicit-interface -fPIC -fmax-errors=1 -g -fcheck=all -fbacktrace  -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
